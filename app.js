91720e2e8451db15d51dc68e6d70133d31f4db29
Himanshu Jain <himanshu14071999@gmail.com>
2020-08-05 13:54:33 +0530

    basic authentication

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 
 const Dishes = require('./models/dishes');
 const mongoose = require('mongoose');
+const { base } = require('./models/dishes');
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
@@ -27,6 +28,31 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req,res,next){
+    console.log(req.headers);
+    var authHeader=req.headers.authorization;
+    if(!authHeader){
+        err = new Error("you are not Authennticated!");
+        res.setHeader('WWW-Authenticate','Basic');
+        err.status = 401;
+        return next(err);
+    }
+    var auth = new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+
+    if (user == 'admin' && pass == 'password') {
+      next(); // authorized
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+}
+
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
